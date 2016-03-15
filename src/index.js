eeb69cd1f5eb83e863f6ee54320a52c684905386
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-03-14 22:21:40 -0700

    create factory functions to create countries and map them to dom nodes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './assets/css/index.scss';
 import dataJSON from 'file!./data/data.json';
 
 const Api = () => {
-	function getData() {
+	const getData = () => {
 		return new Promise((resolve, reject) => {
 			fetch(dataJSON)
 				.then((response) => {
@@ -25,10 +25,74 @@ const Api = () => {
 				.then(response => resolve(response))
 				.catch(error => console.error(error));
 		});
-	}
+	};
 
 	return {
 		getData
 	};
 };
 
+const Country = (name, networks) => {
+	const getName = () => name;
+	const getNetwork = (network) => networks[network];
+
+	return {
+		getName,
+		getNetwork
+	};
+};
+
+const DOMNode = (tag, attributes) => {
+	const element = document.createElement(tag);
+	attributes.forEach((attr) => {
+		element.setAttribute(attr.name, attr.value);
+	});
+
+	return element;
+};
+
+const renderNodes = (nodes) => {
+	const parent = document.querySelector('.app');
+	nodes.forEach(node => parent.appendChild(node));
+};
+
+const App = () => {
+	const state = {
+		countries: []
+	};
+
+	const DOMStuff = (callback) => {
+		const countryNodes = state.countries.map((country) => {
+			return DOMNode('div', [{
+				name: 'id',
+				value: country.getName()
+			}]);
+		});
+
+		callback(countryNodes);
+	};
+
+	// const renderNodes = (nodes) => {
+	// 	const parent = document.querySelector('.app');
+	// 	nodes.forEach(node => parent.appendChild(node));
+	// };
+
+	const init = () => {
+		Api()
+			.getData()
+			.then((response) => {
+				const data = response.data;
+				data.forEach((item) => {
+					state.countries.push(Country(item.name, item.networks));
+				});
+			});
+	};
+
+	return {
+		init,
+		DOMStuff
+	};
+};
+
+App().init();
+App().DOMStuff(renderNodes);
