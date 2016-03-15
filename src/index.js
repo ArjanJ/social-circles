efaade48ccee166882b4ab5a0c01c2b8a68bfaa6
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-03-14 17:57:18 -0700

    create api module to fetch data from json file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('hello');
+if (module.hot) {
+	module.hot.accept();
+}
+
+// Load CSS
+import './assets/css/index.scss';
+
+// Load data file
+import dataJSON from 'file!./data/data.json';
+
+const Api = () => {
+	function getData() {
+		return new Promise((resolve, reject) => {
+			fetch(dataJSON)
+				.then((response) => {
+					if (response.status >= 200 && response.status < 300) {
+						return response;
+					} else {
+						const error = new Error(response.statusText);
+						error.response = response;
+						throw error;
+					}
+				})
+				.then(response => response.json())
+				.then(response => resolve(response))
+				.catch(error => console.error(error));
+		});
+	}
+
+	return {
+		getData
+	};
+};
+
