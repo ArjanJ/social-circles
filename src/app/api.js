1f1a4f6146adbb12a7a918b68724075231825d37
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-03-16 15:12:04 -0700

    simulate async with setTimeout

--- src/app/api.js
+++ src/app/api.js
@@ -1,22 +1,25 @@
-// Load data file
+// Include data file
 import dataJSON from 'file!../data/data.json';
 
 const Api = () => {
 	const getData = () =>
 		new Promise((resolve, reject) => {
-			fetch(dataJSON)
-				.then((response) => {
-					if (response.status >= 200 && response.status < 300) {
-						return response;
-					} else {
-						const error = new Error(response.statusText);
-						error.response = response;
-						throw error;
-					}
-				})
-				.then(response => response.json())
-				.then(response => resolve(response))
-				.catch(error => console.error(error));
+			// Simulate async
+			setTimeout(() => {
+				fetch(dataJSON)
+					.then((response) => {
+						if (response.status >= 200 && response.status < 300) {
+							return response;
+						} else {
+							const error = new Error(response.statusText);
+							error.response = response;
+							throw error;
+						}
+					})
+					.then(response => response.json())
+					.then(response => resolve(response))
+					.catch(error => console.error(error));
+			}, 200);
 		});
 
 	return {
