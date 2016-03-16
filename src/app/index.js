1b52e7d70841afb07703bf49ce7a849f86452642
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-03-16 15:13:23 -0700

    add classes to countries and buttons for styling purposes

--- src/app/index.js
+++ src/app/index.js
@@ -27,7 +27,8 @@ const App = () => {
 			Api()
 				.getData()
 				.then((response) => {
-					state.countries = response.data.map(item => Country(item.name, item.networks));
+					const countries = response.data.map(item => Country(item.name, item.networks));
+					setState({ countries });
 					resolve(state.countries);
 				})
 				.catch(error => console.error(error));
@@ -49,6 +50,8 @@ const App = () => {
 			countries[i].style.transform = `scale(
 				${country.getNetwork(state.network).totalUsers / 100000000}
 			)`;
+			countries[i].classList.remove('facebook', 'instagram', 'twitter');
+			countries[i].classList.add(state.network);
 		});
 	};
 
@@ -56,16 +59,16 @@ const App = () => {
 		const network = button.getAttribute('data-network');
 		const buttons = Array.from(document.querySelectorAll('.controls__button'));
 
-		buttons.map(x => x.classList.remove('controls__button--active'));
-		button.classList.add('controls__button--active');
-
 		setState({ network });
+
+		buttons.map(x => x.classList.remove('facebook', 'instagram', 'twitter'));
+		button.classList.add('controls__button--active', state.network);
+
 		animateCountry();
 	};
 
 	const render = () =>
-		`<div>
-			<h1 class="title">The Social Media Invasion</h1>
+		`<h1 class="title">Social Circles</h1>
 			<div class="earth">
 				<div class="earth__ocean"></div>
 				<div class="earth__map">
@@ -73,7 +76,7 @@ const App = () => {
 						${state.countries.map((country) =>
 							`<div
 								id="${country.getName().replace(/\ /g, '-')}"
-								class="country"
+								class="country ${state.network}"
 								style="transform: scale(
 									${country.getNetwork(state.network).totalUsers / 100000000}
 								)">
@@ -82,11 +85,10 @@ const App = () => {
 				</div>
 			</div>
 			<div class="controls">
-				<button class="controls__button" type="button" data-network="facebook">Facebook</button>
+				<button class="controls__button facebook" type="button" data-network="facebook">Facebook</button>
 				<button class="controls__button" type="button" data-network="instagram">Instagram</button>
 				<button class="controls__button" type="button" data-network="twitter">Twitter</button>
-			</div>
-		</div>`;
+			</div>`;
 
 	const mount = () => {
 		document
